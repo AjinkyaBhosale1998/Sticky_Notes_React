14a845e909fbeedd3eebfab34c7f5f94f1b85590
Ajinkya Bhosale <ajinkya.bhosale@ascendion.com>
2024-02-14 16:59:40 +0530

    commit todo

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Todo from "./components/todo.jsx";
+
+function App() {
+  return (
+    <>
+      <>
+      <Todo/>
+      </>
+    </>
+  );
+}
+
+export default App;
